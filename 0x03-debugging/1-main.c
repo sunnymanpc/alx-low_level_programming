b7fef3b4c4e52460cc3cce254834099ff58d3f4d
sunnymanpc <julianmercy@gmail.com>
2023-06-22 08:38:26 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,15 +8,12 @@ int main(void)
 	int i;
 
 	printf("Infinete loop incoming :(\n");
-
 	i = 0;
-
 	/*while (i<10*/
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i):*/
 	/*}*/
-	printf("Ininite loop avoided!\\o/\n");
-
+	printf("Ininite loop avoided! \\o/\n");
 	return (0);
 }
